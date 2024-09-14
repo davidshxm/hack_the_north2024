f8eeedfb7fd3e957dd2c57b8975ef9a5f33aeac0
David Shim <davidshim0331@gmail.com>
2024-09-14 10:35:18 -0400

    Implement Images to replace buttons

--- lib/inventory.dart
+++ lib/inventory.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math'; // Import dart:math for randomization
 import 'camera.dart';
 import 'inventory_manager.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
@@ -16,7 +15,6 @@ class _InventoryPage extends State<Inventory> {
   PanelController _pc = PanelController();
   bool _isPanelVisible = false;
   String value = ''; // Initially set to an empty string
-  final Random _random = Random(); // Create an instance of Random for random image selection
 
   @override
   void initState() {
@@ -24,13 +22,13 @@ class _InventoryPage extends State<Inventory> {
   }
 
   // Function to select a random image
-  String _getRandomImage() {
+  String _getRandomImage(int index) {
     List<String> images = [
       'assets/GameBoy-2.png',
       'assets/Card-2.png',
       'assets/Tamagotchi-2.png',
     ];
-    return images[_random.nextInt(images.length)]; // Randomly pick an image from the list
+    return images[index % images.length]; // Randomly pick an image from the list
   }
 
   @override
@@ -58,7 +56,9 @@ class _InventoryPage extends State<Inventory> {
           });
         },
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: _isPanelVisible
+          ? null // Hide FAB when the panel is visible
+          : FloatingActionButton(
         onPressed: () async {
           // Navigate to the camera page when clicked
           Navigator.pushReplacement(
@@ -69,6 +69,11 @@ class _InventoryPage extends State<Inventory> {
           );
         },
         child: Image.asset('assets/PlusButton.png'),
+        elevation: 0, // Remove shadow
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30), // Adjust the border radius if needed
+        ),
+        backgroundColor: Colors.transparent, // Make background transparent to remove border effect
       ),
     );
   }
@@ -107,7 +112,7 @@ class _InventoryPage extends State<Inventory> {
                               children: [
                                 // IconButton with a random image
                                 IconButton(
-                                  icon: Image.asset(_getRandomImage()), // Use random image for each button
+                                  icon: Image.asset(_getRandomImage(index)), // Use random image for each button
                                   iconSize: 100, // Adjust the icon size as needed
                                   onPressed: () {
                                     setState(() {
